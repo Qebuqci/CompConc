9907a2fcb010a81cb6a8fd562b6c827a34d19573
Ricardo <ricardokae.rj@gmail.com>
2022-01-12 00:57:55 -0300

    Lab 6

--- Lab 6/lab6.c
+++ Lab 6/lab6.c
@@ -12,7 +12,7 @@ int contador_barreira = 0;
 pthread_mutex_t lock;
 pthread_cond_t cond_barreira;
 
-void barreira()
+void barreira(int id_thread)
 {
     // Entra na seção crítica
     pthread_mutex_lock(&lock);
@@ -20,10 +20,10 @@ void barreira()
     if (contador_barreira == NTHREADS - 1) {
         contador_barreira = 0;
         pthread_cond_broadcast(&cond_barreira);
-        //printf("Thread: %d liberou a barreira\n", id_thread);
+        printf("Thread: %d liberou a barreira\n", id_thread);
     }
     else {
-        //printf("Thread: %d na barreira\n", id_thread);
+        printf("Thread: %d na barreira\n", id_thread);
         contador_barreira++;
         pthread_cond_wait(&cond_barreira, &lock);
     }
@@ -33,53 +33,49 @@ void barreira()
 
 void* soma(void* arg)
 {
-    int *acc; 
-    int id; 
+    long int *acc; 
+    long int id = (long int) arg;
+   
     
-    id = *((int *) arg);
-    acc = malloc(sizeof(long long int));    
+    acc = (long int *) malloc(sizeof(long int));    
     
     for (int i = 0; i < NTHREADS; i++) {
-        //printf("Iteração: %d\tThread: %d executando \n", i, id);
-        for (int j = 0; j < NTHREADS; j++) {
+        printf("Iteração: %d\tThread: %d\n", i, id);
+        for (int j = 0; j < NTHREADS; j++) 
             *acc += vetor[j];
-        }
-        //printf("\n");
+        
         barreira(id);
-        vetor[id] = 3 * (id);
+        vetor[id] = 3 * i;
         barreira(id);
     }
     
-     pthread_exit((void *) acc);
+    pthread_exit((void *) acc);
 }
 
 int main(void)
 {
     pthread_t threads[NTHREADS];
-    int **acc_thread;
-    //int tid_local[NTHREADS];
+    long int acc_thread;
         
     // 1. Inicialização do vetor
     for (int i = 0; i < NTHREADS; i++)
         vetor[i] = 2 * i;
     
     // 2. Criação das threads
-    for (int i = 0; i < NTHREADS; i++) {
-        //tid_local[i] = i;
-        if (pthread_create(threads + i, NULL, soma, (void *) &i)) {
+    for (long int i = 0; i < NTHREADS; i++) {
+        if (pthread_create(threads + i, NULL, soma, (void *) i)) {
             puts("ERRO: pthread_create()");
             return ERRO;
         }
     }
     
     // 3. Espera as threads terminarem
-    for (int i = 0; i < NTHREADS; i++) {
-        if (pthread_join(threads[i], (void **) acc_thread)) {
+   for (int i = 0; i < NTHREADS; i++) {
+        if (pthread_join(threads[i], (void **) &acc_thread)) {
             puts("ERRO: pthread_join()");
             return ERRO;            
         }
-        printf("Thread: %d \t Valor recebido: %d\n", i, *(*acc_thread));
-        free(*acc_thread);
+        printf("Thread: %d \t Valor recebido: %d\n", i, *((int *) acc_thread));
     }
     
     // 4. Desalocação de recursos
